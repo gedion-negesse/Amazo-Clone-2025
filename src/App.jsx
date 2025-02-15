ab3447369efc1acfcb6325386f28b9373ba8f2dd
gedion <legessegedion@gmail.com>
2025-02-15 12:36:50 -0500

    carousle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import "./App.css";
-
+import Carusole from "./components/Carusole/Carusole";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <Header />
+      <Carusole />
     </>
   );
 }
---
+++ src/components/Carusole/Image/data.js
@@ -0,0 +1,7 @@
+import img1 from "./10001.jpg";
+import img2 from "./imag1.jpg";
+import img3 from "./image2.jpg";
+import img4 from "./image3.jpg";
+import img5 from "./image4.jpg";
+
+export const imageList = [img1, img2, img3, img4, img5];
