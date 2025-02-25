a6bad0786951f67780b007e05e638d79e94df566
gedion <legessegedion@gmail.com>
2025-02-25 15:57:50 -0500

    firebase and function

---
+++ functions/index.js
@@ -0,0 +1,43 @@
+const { onRequest } = require("firebase-functions/v2/https");
+
+const logger = require("firebase-functions/logger");
+
+const express = require("express");
+
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const STRIPE_KEY = process.env.STRIPE_KEY;
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "success",
+  });
+});
+
+exports.api = onRequest(app);
+
+app.post("/payment/creat", async (req, res) => {
+  const total = req.query.total;
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+
+    res.status(201).json({
+      clienSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(403).json({
+      message: "tota must be graeter than 0 ",
+    });
+  }
+});
