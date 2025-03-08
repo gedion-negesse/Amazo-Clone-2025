9c82e7479677cca3da2a10a8068d1599ba657e48
gedion <legessegedion@gmail.com>
2025-03-07 20:04:41 -0500

    deploy

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   // local instance of firebase functions url
-  baseURL: "http://127.0.0.1:5001/clone-2025-7f12b/us-central1/api",
+  //baseURL: "http://127.0.0.1:5001/clone-2025-7f12b/us-central1/api",
 
   //deployed version of Amazon-clone-2025 server on firebase function
-  //baseURL: "https://api-meg555guhq-uc.a.run.app/",
+  baseURL: "https://api-meg555guhq-uc.a.run.app/",
 
   //deployed version of amzon-api server on render.com
   //baseURL: "https://amazon-api-eu5z.onrender.com/",
